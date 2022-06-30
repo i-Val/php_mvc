4bf2c55219b560c5431fd7eab373afa397fccfd9
Iwuchukwu valentine <69054588+i-Val@users.noreply.github.com>
2022-06-30 09:59:15 +0100

    added mail config

---
+++ app/classes/Mail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\classes;
+
+use PHPMailer;
+
+class Mail
+{
+    protected $mail;
+
+    public function __construct(){
+        $this->mail = new PHPMailer;
+        $this->setUp();
+    }
+
+    public function setup(){
+        $this->mail->isSMTP();
+        $this->mail->Mailer = 'smtp';
+        $this->mail->SMTPAuth = true;
+        $this->mail->SMTPSecure = 'tls';
+
+        $this->mail->Host = $_ENV['SMTP_HOST'];
+        $this->mail->Port = $_ENV['SMTP_PORT'];
+
+        $environment = $_ENV['APP_ENV'];
+
+        if($nvironment === 'local'){
+            $this->mail->SMTPDebug = 2;
+        }
+
+        //auth info
+        $this->mail->Username = $_ENV['EMAIL_USERNAME'];
+        $this->mail->Password = $_ENV['EMAIL_PASSWORD'];
+
+        $this->mail->isHTML(true);
+        $this->mail->SingleTo = true;
+
+        //sender info
+        $this->mail->From = $_ENV['ADMIN_EMAIL'];
+        $this->mail->FromName = 'entropy';
+    }
+
+    public function send($data){
+        $this->mail->addAddress($data['to'], $data['name']);
+        $this->mail->Subject = $data['subject'];
+        $this->mail->Body = $body;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -8,3 +8,4 @@ $app_name = $_ENV["APP_NAME"];
 
 $user = Capsule::table('users')->where('id', 1)->first();
 var_dump($user);
+
